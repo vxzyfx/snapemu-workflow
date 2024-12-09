1c238ba2881fb37a56b4b6a6896f5c9435baf15a
shug <vxshug@gmail.com>
2024-12-09 22:08:35 +0800

    Update setup.sh

--- .github/setup.sh
+++ .github/setup.sh
@@ -3,6 +3,6 @@ wget https://musl.cc/aarch64-linux-musl-cross.tgz
 tar xf aarch64-linux-musl-cross.tgz
 
 export PATH=$PWD/aarch64-linux-musl-cross/bin:$PATH
-cargo build  --target=aarch64-unknown-linux-musl
+cargo build  --target=aarch64-unknown-linux-musl --release
 mkdir bin/
 cp target/aarch64-unknown-linux-musl/debug/snap_api bin/
