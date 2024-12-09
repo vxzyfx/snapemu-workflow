6af3599ab86913b740e55ecb0318d41305415715
shug <vxshug@gmail.com>
2024-12-09 20:22:35 +0800

    Update setup.sh

--- .github/setup.sh
+++ .github/setup.sh
@@ -3,3 +3,4 @@ wget https://musl.cc/aarch64-linux-musl-cross.tgz
 tar xf aarch64-linux-musl-cross.tgz
 
 export PATH=$PWD/aarch64-linux-musl-cross/bin:$PATH
+cargo build  --target=aarch64-unknown-linux-musl
