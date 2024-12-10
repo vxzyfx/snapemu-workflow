9b93997a8f0016c39d108e45051d13472a6b92d4
vxzyfx's github <vxzyfx@gmail.com>
2024-12-10 20:29:20 +0800

    Delete .github/setup.sh

--- .github/setup.sh
+++
@@ -1,8 +0,0 @@
-echo Setup
-wget https://musl.cc/aarch64-linux-musl-cross.tgz
-tar xf aarch64-linux-musl-cross.tgz
-
-export PATH=$PWD/aarch64-linux-musl-cross/bin:$PATH
-cargo build  --target=aarch64-unknown-linux-musl --release
-mkdir bin/
-cp target/aarch64-unknown-linux-musl/release/snap_api bin/
